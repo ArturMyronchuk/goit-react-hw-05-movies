b15edc6439bd5da29e5fe3cd67e4b310d720f068
Artur Myronchuk <122921226+ArturMyronchuk@users.noreply.github.com>
2023-11-02 22:16:23 +0200

    fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,36 +1,35 @@
-const API_KEY='3073a492602eee53f225e37916e4cfb6';
-const BASE_URL='https://api.themoviedb.org/3';
+const API_KEY = '8b8fbecbc585ee4df16b8b3d4e4beedc';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const responses = {
-  async fetchPopularMovies ()  {
+  async fetchPopularMovies() {
     const response = await fetch(
       `${BASE_URL}/trending/all/day?api_key=${API_KEY}`
     );
     return await response.json();
   },
-  async fetchMovieById (movieId)  {
+  async fetchMovieById(movieId) {
     const response = await fetch(
       `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`
     );
     return await response.json();
   },
-  async fetchMovieBySearch (searchQuery)  {
+  async fetchMovieBySearch(searchQuery) {
     const response = await fetch(
       `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`
     );
     return await response.json();
   },
-  async fetchMovieByCast (movieId)  {
+  async fetchMovieByCast(movieId) {
     const response = await fetch(
       `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
     );
     return await response.json();
   },
-  async fetchMovieByReviews (movieId)  {
+  async fetchMovieByReviews(movieId) {
     const response = await fetch(
       `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
     );
     return await response.json();
   },
-}
-
+};
